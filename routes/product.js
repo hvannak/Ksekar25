19790b1cbd022de5843db8871b16216b935ad6ba
hvannak <vannak2010@gmail.com>
2021-08-09 16:17:54 +0700

    thinking of search products

--- routes/product.js
+++ routes/product.js
@@ -14,6 +14,40 @@ router.post('/page',verify,async (req,res) => {
     }   
 });
 
+router.post('/search',async (req,res) => {
+    try{
+        var reqData = req.body;
+        let dynamicQuery = {};
+
+        if(req.body.categoryId != '0'){
+            dynamicQuery["category"] = reqData.category
+        }
+        if(filterObj.title != ""){
+            dynamicQuery["title"] = { "$regex": reqData.title, "$options": "i" }
+        }
+        if(filterObj.description != ""){
+            dynamicQuery["description"] = { "$regex": reqData.description, "$options": "i" }
+        }
+
+        if(reqData.fromprice && reqData.toprice){
+            dynamicQuery["price"] = { $lte: reqData.toprice, $gte: reqData.fromprice }
+            if(filterObj.currency != "0"){
+                dynamicQuery["currency"] = reqData.currency
+            }
+        }
+        console.log(dynamicQuery);
+        docObj = await Product.find({$and:[dynamicQuery]}).limit(reqData.pageSize).skip(reqData.pageSize*(reqData.page-1)).sort({
+            date: 'desc'
+        });
+
+        var totalItems = await Product.count({$and:[dynamicQuery]});
+        res.json({objList:docObj,totalDoc:totalItems});       
+    }catch(err){
+        logger.error('product page:' + err);
+        res.json(err);
+    }   
+});
+
 router.get('/props', async (req,res) => {
     try{
         const props =  Object.keys(Product.schema.paths);
@@ -36,7 +70,7 @@ router.post('/post',verify,async (req,res)=> {
     });
     try{
         await docObj.save();
-        let resObj = await Product.find({ _id: docObj._id }).populate('category');
+        let resObj = await Product.find({ _id: docObj._id }).populate('category').populate('currency');
         res.json({obj:resObj[0],message:'INSERT'});
     } catch(err) {
         logger.error('product post:' + err);
@@ -67,7 +101,7 @@ router.put('/put/:proId',verify, async (req,res) => {
             image: req.body.image
         });
         await Product.update(filter,update);
-        let resObj = await Product.find(filter).populate('lang');
+        let resObj = await Product.find(filter).populate('category').populate('currency');
         res.json({obj:resObj[0],message:'UPDATE'});
     }catch(err){
         logger.error('category put:' + err);
